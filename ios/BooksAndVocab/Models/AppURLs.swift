401997bbf2b8841eb6730989b7236737a0180b51
Max0228 <max970228@gmail.com>
2026-06-09 16:40:15 +0800

    ios: centralize external identity literals

--- ios/BooksAndVocab/Models/AppURLs.swift
+++ ios/BooksAndVocab/Models/AppURLs.swift
@@ -1,7 +1,7 @@
 import Foundation
 
 enum AppURLs {
-    static let domain = "https://wordnexus.lol"
+    static let domain = BrandIdentity.publicBaseURL
     // swiftlint:disable force_unwrapping
     static let privacy = URL(string: "\(domain)/privacy.html")!
     static let terms = URL(string: "\(domain)/terms.html")!
