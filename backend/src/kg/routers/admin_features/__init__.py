6b59c369ee0aa574f630803f8cfd732d5c10955f
Max0228 <max970228@gmail.com>
2026-08-13 02:45:08 +0800

    api: pin admin route compatibility surface
    Reviewed-by: delivery_team_b_supervisor

--- backend/src/kg/routers/admin_features/__init__.py
+++ backend/src/kg/routers/admin_features/__init__.py
@@ -4,6 +4,7 @@ from .registry import (
     ADMIN_API_ROUTE_REGISTRY,
     AdminRouteRegistration,
     BoundAdminRoute,
+    REQUIRED_ADMIN_API_ROUTE_KEYS,
     iter_bound_admin_api_routes,
     validate_admin_route_registry,
 )
@@ -12,6 +13,7 @@ __all__ = [
     "ADMIN_API_ROUTE_REGISTRY",
     "AdminRouteRegistration",
     "BoundAdminRoute",
+    "REQUIRED_ADMIN_API_ROUTE_KEYS",
     "iter_bound_admin_api_routes",
     "validate_admin_route_registry",
 ]
