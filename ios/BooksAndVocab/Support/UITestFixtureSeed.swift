8a474582b90a849f19a79e50770759d173987277
Max0228 <max970228@gmail.com>
2026-08-13 03:37:06 +0800

    ios: refactor P10 stats projection and UI world selectors
    (cherry picked from commit 33af8ed23cb0a0c423a7f70af3a0242b893fa1cf)

--- ios/BooksAndVocab/Support/UITestFixtureSeed.swift
+++ ios/BooksAndVocab/Support/UITestFixtureSeed.swift
@@ -73,6 +73,8 @@ enum UITestFixtureSeed {
             case "explore":
                 seedExplore(id, into: container)
 #endif
+            case "vocabulary":
+                seedVocabulary(id, into: container)
             case "entitlements":
                 guard id == "pro" || id == "free" else {
                     failFixtureSeed("Unknown entitlements fixture ID: \(id)")
